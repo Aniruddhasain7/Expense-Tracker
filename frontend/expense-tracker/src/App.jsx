b589b68a982e182f6c23112e381f41b9b3491a2f
Aniruddha Sain <aniruddhasain315@gmail.com>
2026-03-24 21:49:49 +0530

    update

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,51 +1,70 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
+  useNavigate,
 } from "react-router-dom";
+
 import Login from "./pages/Auth/Login";
 import SignUp from "./pages/Auth/SignUp";
 import Home from "./pages/Dashboard/Home";
 import Income from "./pages/Dashboard/Income";
 import Expense from "./pages/Dashboard/Expense";
-import UserProvider from "./context/UserContext";
-import {Toaster} from 'react-hot-toast'
 
-import toast from "react-hot-toast";
+import UserProvider from "./context/UserContext";
+import { Toaster } from "react-hot-toast";
+
+
+import { setNavigate } from "./utils/axiosInstance";
+
 
 const App = () => {
   return (
     <UserProvider>
-      <div>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Root />} />
-            <Route path="/login" exact element={<Login />} />
-            <Route path="/signup" exact element={<SignUp />} />
-            <Route path="/dashboard" exact element={<Home />} />
-            <Route path="/income" exact element={<Income />} />
-            <Route path="/expense" exact element={<Expense />} />
-          </Routes>
-        </Router>
-      </div>
+      <Router>
+        <AppContent /> 
+      </Router>
+
       <Toaster
-       toastOptions={{
-        className: "",
-        style: {
-          fontSize: '13px'
-        }
-       }}
-       />
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
     </UserProvider>
   );
 };
 
 export default App;
+
+
+const AppContent = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setNavigate(navigate); 
+  }, [navigate]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Root />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/dashboard" element={<Home />} />
+      <Route path="/income" element={<Income />} />
+      <Route path="/expense" element={<Expense />} />
+    </Routes>
+  );
+};
+
+
 const Root = () => {
   const isAuthenticated = !!localStorage.getItem("token");
+
   return isAuthenticated ? (
     <Navigate to="/dashboard" />
   ) : (
