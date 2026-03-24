b97c5b17d265a2f72a91d60eff61298e14cbccd4
Aniruddha Sain <aniruddhasain315@gmail.com>
2026-03-24 20:24:52 +0530

    update

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -2,30 +2,30 @@ export const BASE_URL = "https://expense-tracker-backend-9wpd.onrender.com";
 
 export const API_PATHS = {
   AUTH: {
-    LOGIN: "/api/v1/auth/login",
-    REGISTER: "/api/v1/auth/register",
-    GET_USER_INFO: "/api/v1/auth/getUser",
+    LOGIN: `${BASE_URL}/api/v1/auth/login`,
+    REGISTER: `${BASE_URL}/api/v1/auth/register`,
+    GET_USER_INFO: `${BASE_URL}/api/v1/auth/getUser`,
   },
 
   DASHBOARD: {
-    GET_DATA: "/api/v1/dashboard",
+    GET_DATA: `${BASE_URL}/api/v1/dashboard`,
   },
 
   INCOME: {
-    ADD_INCOME: "/api/v1/income/add",
-    GET_ALL_INCOME: "/api/v1/income/get",
-    DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
-    DOWNLOAD_INCOME: "/api/v1/income/downloadexcel",
+    ADD_INCOME: `${BASE_URL}/api/v1/income/add`,
+    GET_ALL_INCOME: `${BASE_URL}/api/v1/income/get`,
+    DELETE_INCOME: (id) => `${BASE_URL}/api/v1/income/${id}`,
+    DOWNLOAD_INCOME: `${BASE_URL}/api/v1/income/downloadexcel`,
   },
 
   EXPENSE: {
-    ADD_EXPENSE: "/api/v1/expense/add",
-    GET_ALL_EXPENSE: "/api/v1/expense/get",
-    DELETE_EXPENSE: (expenseId) => `/api/v1/expense/${expenseId}`,
-    DOWNLOAD_EXPENSE: "/api/v1/expense/downloadexcel",
+    ADD_EXPENSE: `${BASE_URL}/api/v1/expense/add`,
+    GET_ALL_EXPENSE: `${BASE_URL}/api/v1/expense/get`,
+    DELETE_EXPENSE: (id) => `${BASE_URL}/api/v1/expense/${id}`,
+    DOWNLOAD_EXPENSE: `${BASE_URL}/api/v1/expense/downloadexcel`,
   },
 
   IMAGE: {
-    UPLOAD_IMAGE: "/api/v1/auth/upload-image",
+    UPLOAD_IMAGE: `${BASE_URL}/api/v1/auth/upload-image`,
   },
 };
